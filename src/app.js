ac0b12651e1d570cd693702fb5458c686864bf7e
Gunjan Agrawal <gunjan11223374@gmail.com>
2024-02-22 15:33:33 +0530

    added cookie-parser and cors and configured them

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
 import express from "express";
+import  cors from "cors"
+import cookieParser from "cookie-parser";
 const app=express();
+app.use(cors(
+   { origin: process.env.CORS_ORIGIN,
+    credentials:true
+}
+))
+//applying the limit for json files and adding major configuration
+app.use(express.json({limit:"16kb"}));
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.static);
+app.use(cookieParser());
 export{app}
+//generally we have a requeste and a response 
