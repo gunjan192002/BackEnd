960083c5f016f62bc8f46b6f4acf2538d2662741
Gunjan Agrawal <gunjan11223374@gmail.com>
2024-02-23 18:58:48 +0530

    register login is working checked on postman

--- src/db/index.js
+++ src/db/index.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-const connectDB=async()=>{{
-    try{
-
-       const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}`);
-        console.log('\n MongoDB connected !! DB HOST:${connectionInstance.host}, DB NAME: ${connectionInstance.name}\n');
-    }catch(error){
-        console.log("mongodb connection error",error);
-        process.exit(1);
+const connectDB = async () => {
+    try {
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}`);
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
     }
-}}
-export default  connectDB;
+}
+
+export default connectDB
--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import dotenv from "dotenv";
 
 import connectDB from "./db/index.js"
 dotenv.config({ path: './env' });
+import {app} from './app.js'
 // when mongoose finishes connecting to the server it returns a  promise, so we use .then() method on that promise and pass in an arrow function as its
-connectDB().then(()=>{
-    app.listen(process.env.PORT||8000,()=>{
-        console.log('Server is runnign ar port :', process.env.PORT || 8000);
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
-}).catch((err)=>
-{console.log("MONGO db connection failde!!!!",err)});
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
