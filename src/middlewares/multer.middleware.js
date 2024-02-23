4d04ffc94b50e670152aa5f311c2ef9cf83daf20
Gunjan Agrawal <gunjan11223374@gmail.com>
2024-02-23 17:21:27 +0530

    add the user controlers to register

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,19 @@
 import multer from "multer";
 
+// the below part of the code is as it is taken from multer site to uplad a single file 
+//from the device to storage and multer acts a middle ware
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./public/temp")
     },
+    //check what are the different option present for 
+    //the option of file
     filename: function (req, file, cb) {
       
       cb(null, file.originalname)
     }
   })
-  
+  // we are using disc storage here 
 export const upload = multer({ 
     storage, 
 })
